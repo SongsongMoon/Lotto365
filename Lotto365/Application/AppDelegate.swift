e71b2eb3b521c094dbb19282631f8df09c8b7dbc
SongsongMoon <kyusong12@gmail.com>
2020-05-18 19:07:35 +0900

    create directory

---
+++ Lotto365/Application/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Lotto365
+//
+//  Created by Song on 18/05/2020.
+//  Copyright © 2020 FACTORY X. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
